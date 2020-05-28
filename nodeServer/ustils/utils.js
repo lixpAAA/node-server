63f1375d52dafc1f3633afb19a292b60b5d81587
lixpAAA <18040048434@163.com>
2020-05-28 18:20:09 +0800

    Update utils.js

--- nodeServer/ustils/utils.js
+++ nodeServer/ustils/utils.js
@@ -4,3 +4,25 @@ export function DeepClone(obj) {
   for (var key in obj) cpObj[key] = DeepClone(obj[key]);
   return cpObj;
 }
+export function isObjEqual(obj1, obj2) {
+  if ((obj1 === null && obj2 === null) || (typeof obj1 !== 'object' && typeof obj2 !== 'object')) {
+    if (obj1 === obj2) return true
+  }
+  if (obj1 instanceof Array && obj2 instanceof Array) {
+    if (obj1.length !== obj2.length) return false
+    else {
+      for (let key in obj1) {
+        return isObjEqual(obj1[key], obj2[key])
+      }
+    }
+  }
+  if (obj1 instanceof Object && obj2 instanceof Object) {
+    if (Object.getOwnPropertyNames(obj1).length !== Object.getOwnPropertyNames(obj2).length) return false
+    else {
+      for (let key in obj1) {
+        return isObjEqual(obj1[key], obj2[key])
+      }
+    }
+  }
+  return false
+}
